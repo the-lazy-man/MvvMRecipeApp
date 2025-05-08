f4111f15f77a084885361a5a7df9b71e132490a3
ankit-internetsoft <ankit@internetsoft.site>
2025-05-08 20:09:30 +0530

    first Commit - HomeFragment Complete

--- MVVMprojects/testMVVM/app/src/main/java/com/example/testmvvm/domain/UserViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.testmvvm.domain
-
-import androidx.lifecycle.ViewModel
-
-class UserViewModel(private val getUsersUseCase: GetUsersUseCase) : ViewModel() {
-    var vmUserList = getUsersUseCase.userList
-    val users = getUsersUseCase.execute()
-}
