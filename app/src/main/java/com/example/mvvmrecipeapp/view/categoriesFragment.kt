f20ccfdc9f33df5089cdcd34d53b25d0c6662ba9
the-lazy-man <ankittiwari28032000@gmail.com>
2025-05-15 02:29:30 +0530

    Category fragment done

--- app/src/main/java/com/example/mvvmrecipeapp/view/categoriesFragment.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/view/categoriesFragment.kt
@@ -1,24 +1,68 @@
 package com.example.mvvmrecipeapp.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.mvvmrecipeapp.R
+import com.example.mvvmrecipeapp.adapters.CategoryListAdapter
+import com.example.mvvmrecipeapp.databinding.FragmentCategoriesBinding
+import com.example.mvvmrecipeapp.domain.HomeViewmodel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class categoriesFragment : Fragment() {
+    private lateinit var binding: FragmentCategoriesBinding
+    private lateinit var viewmodel: HomeViewmodel
+    private lateinit var  categoryListAdapter: CategoryListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewmodel = (activity as MainActivity).viewModel
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false)
+        binding = FragmentCategoriesBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        prepareRecyclerView()
+        observeCategories()
+        onCategoryClick()
+    }
+
+    private fun onCategoryClick() {
+        categoryListAdapter.onCategoryItemClick = {category ->
+            val intent = Intent(activity,CategoryMealsActivity::class.java)
+            intent.putExtra(homeFragment.CATEGORY_NAME,category.strCategory)
+            startActivity(intent)
+        }
+    }
+
+    private fun observeCategories() {
+        viewmodel.categoriesLiveData.observe(viewLifecycleOwner, Observer {categories->
+            categoryListAdapter.setCategoryList(categories as ArrayList)
+        })
+    }
+
+    private fun prepareRecyclerView() {
+        categoryListAdapter = CategoryListAdapter()
+        binding.rvCategories.apply {
+            layoutManager = GridLayoutManager(activity,3,GridLayoutManager.VERTICAL,false)
+            adapter = categoryListAdapter
+        }
     }
 }
